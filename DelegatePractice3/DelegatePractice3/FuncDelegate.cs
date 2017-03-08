94210f810a857b6a8f0aa7d68cb882f5de00290f
Ramya P S <ramya.sudarshan1994@gmail.com>
2017-03-08 18:32:53 +0530

    Recharge on Low balance using Func Delegates and lambda expressions.

--- DelegatePractice3/DelegatePractice3/FuncDelegate.cs
+++ DelegatePractice3/DelegatePractice3/FuncDelegate.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelegatePractice2
+namespace DelegatePractice3
 {
     public delegate bool IsRechargableDelegate(Employee emp);
     public class Program
@@ -18,40 +18,50 @@ namespace DelegatePractice2
             empList.Add(new Employee() { EMPId = 103, Name = "Jaanu", Experience = 5, Salary = 30000, Balance = 110 });
             empList.Add(new Employee() { EMPId = 104, Name = "Jenny", Experience = 7, Salary = 60000, Balance = 4 });
             empList.Add(new Employee() { EMPId = 105, Name = "Jeni", Experience = 3, Salary = 50000, Balance = 3 });
-            IsRechargableDelegate isRechargable = new IsRechargableDelegate(Vodafone.RechargeForVodafone);
-            IsRechargableDelegate isRechargable1 = new IsRechargableDelegate(Airtel.RechargeForAirtel);
-            Telecom.RechargeBonus(empList, isRechargable);
+                   
+            Func<Employee, Employee> isRechargable2 = Vodafone.RechargeForVodafone;
+            Func<Employee, Employee> isRechargable3 = Airtel.RechargeForAirtel;
+            Telecom.RechargeBonus(empList, isRechargable2);
+            Telecom.RechargeBonus(empList, isRechargable3);
+            Telecom.RechargeBonus(empList, emp => Vodafone.RechargeForVodafone(emp));
             Console.WriteLine("--------------------------");
-            Telecom.RechargeBonus(empList, isRechargable1);
+            Telecom.RechargeBonus(empList, emp => Airtel.RechargeForAirtel(emp));
             Console.ReadLine();
         }
 
     }
     public class Vodafone
     {
-        public static bool RechargeForVodafone(Employee emp)
+        public static Employee RechargeForVodafone(Employee emp)
         {
             if (emp.Balance == 0)
             {
                 emp.Balance += 10;
-                return true;
+                emp.IsEligibleForBonus = true;
             }
             else
-                return false;
+            {
+                emp.IsEligibleForBonus = false;
+            }
+            return emp;
         }
     }
     public class Airtel
     {
-        public static bool RechargeForAirtel(Employee emp)
+        public static Employee RechargeForAirtel(Employee emp)
         {
             if (emp.Balance <= 5)
             {
                 emp.Balance += 10;
-                return true;
+                emp.IsEligibleForBonus = true;
             }
             else
-                return false;
+            {
+                emp.IsEligibleForBonus = false;
+            }
+            return emp;
         }
+
     }
     public class Employee
     {
@@ -60,18 +70,21 @@ namespace DelegatePractice2
         public double Salary { get; set; }
         public int Experience { get; set; }
         public int Balance { get; set; }
+        public bool IsEligibleForBonus { get; set; }
     }
 
     public class Telecom
     {
         public int EMPId { get; set; }
 
-        public static void RechargeBonus(List<Employee> employees, IsRechargableDelegate isEligibleToRechargeBonus)
+        public static void RechargeBonus(List<Employee> employees, Func<Employee, Employee> isEligibleToRechargeBonus)
         {
             foreach (Employee emp in employees)
             {
-                if (isEligibleToRechargeBonus(emp))
-                    Console.WriteLine("Congratualations {0}!! You have been recharged!!", emp.Name);
+                var emp1=isEligibleToRechargeBonus(emp);
+                if(emp1.IsEligibleForBonus)
+                    Console.WriteLine("Congratualations {0}!! You have been recharged!!", emp1.Name);
+
             }
         }
     }
